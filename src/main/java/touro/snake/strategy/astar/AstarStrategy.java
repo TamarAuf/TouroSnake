11475566de4aaded1eb2fb142177fa7d09d72fec
Tamar <tamartda@gmail.com>
2020-10-27 16:51:37 -0400

    improved strategy

--- src/main/java/touro/snake/strategy/astar/AstarStrategy.java
+++ src/main/java/touro/snake/strategy/astar/AstarStrategy.java
@@ -20,7 +20,6 @@ public class AstarStrategy implements SnakeStrategy {
 
         ArrayList<Node> open = new ArrayList<>();
         ArrayList<Node> closed = new ArrayList<>();
-        ArrayList<Node> successorNodes = new ArrayList<>();
 
         Node node = new Node(head, currentNode, food);
         Node parent = node.getParent();
@@ -29,23 +28,32 @@ public class AstarStrategy implements SnakeStrategy {
         Node lowestNode = open.get(0);
 
         while (!currentNode.equals(goalNode) || !open.isEmpty()) {
-            for (int i = 0; i < directions.length; i++) {
-                if (snake.canTurnTo(directions[i])) {
-                    successorNodes.add((Node) head.moveTo(directions[i]));
-                }
-            }
-            open.addAll(successorNodes);
-            
             for (Node possibleNode : open) {
                 if (possibleNode.getCost() < lowestNode.getCost()) {
                     lowestNode = possibleNode;
                 }
             }
-
             closed.add(lowestNode);
-            open.remove(lowestNode);
 
-
+            for (int i = 0; i < directions.length; i++) {
+                Direction direction = directions[i];
+                Node successorNode = (Node) head.moveTo(directions[i]);
+                if (snake.canTurnTo(direction) && !closed.contains(successorNode)) {
+                    if(!open.contains(successorNode)){
+                        open.add(successorNode);
+                        currentNode = parent;
+                        if (successorNode.getCost() < lowestNode.getCost()) {
+                            lowestNode = successorNode;
+                        }
+                    }
+                    else {
+                        if (successorNode.getCost() < lowestNode.getCost()) {
+                            lowestNode = successorNode;
+                        }
+                        Node newNode = new Node(lowestNode, currentNode, food);
+                    }
+                }
+            }
         }
     }
 }
